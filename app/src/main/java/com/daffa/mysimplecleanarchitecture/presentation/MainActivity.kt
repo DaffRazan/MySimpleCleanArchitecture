04ad9ba605fafc586bf08bf8558aa5da242ca6e9
Daffa Razan <daffarazan17@gmail.com>
2021-05-28 08:43:42 +0700

    Test error message

--- app/src/main/java/com/daffa/mysimplecleanarchitecture/presentation/MainActivity.kt
+++ app/src/main/java/com/daffa/mysimplecleanarchitecture/presentation/MainActivity.kt
@@ -19,7 +19,7 @@ class MainActivity : AppCompatActivity() {
         val factory = MainViewModelFactory.getInstance()
         val viewModel = ViewModelProvider(this, factory)[MainViewModel::class.java]
 
-        viewModel.setName("Daffa")
+        viewModel.setName("Daffarazan")
         viewModel.message.observe(this, Observer {
             binding.tvWelcome.text = it.welcomeMessage
         })
